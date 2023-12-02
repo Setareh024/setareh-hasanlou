3b2db07da3122cd336a83fba6b4dc7f87163cd9f
Setareh024 <152712050+Setareh024@users.noreply.github.com>
2023-12-02 19:05:29 +0330

    Add files via upload

---
+++ mbweek4.cpp 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+  main() {   
+
+ float a,b;
+ cout<<"please enter2 float number";
+ cout<<" enter a:";
+ cin>>a;
+ cout<<"enter b:";
+ cin>>b;
+ cin>>a>>b;
+ cout<<"a+b:";
+ cin>>a>>b;
+ cout<<"a-b:";
+  cin>>a>>b;
+ cout<<"a*b:";
+int x =(1%2);
+
+ cin>>a>>b;
+cout<<"1%2:";
+ "y=1";  "x=y%2";
+  
+}
